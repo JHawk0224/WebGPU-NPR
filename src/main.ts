169696528c2c286666f53eaa587236fb3d0d2ecb
JHawk0224 <jhawk111@icloud.com>
2024-11-25 16:47:51 -0500

    Nuke old unused code

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import Stats from "stats.js";
 import { GUI } from "dat.gui";
 
 import { initWebGPU, Renderer } from "./renderer";
-import { NaiveRenderer } from "./renderers/naive";
-import { ClusteredDeferredRenderer } from "./renderers/clustered_deferred";
 import { Pathtracer } from "./renderers/pathtracer";
 
 import { setupLoaders, Scene } from "./stage/scene";
-import { Lights } from "./stage/lights";
 import { Camera } from "./stage/camera";
 import { Stage } from "./stage/stage";
 import { vec3 } from "wgpu-matrix";
@@ -44,22 +41,14 @@ let scene = new Scene();
 // await scene.loadGltf("./scenes/sponza/Sponza.gltf"); // too big for storage
 
 const camera = new Camera();
-const lights = new Lights(camera);
 
 const stats = new Stats();
 stats.showPanel(0);
 document.body.appendChild(stats.dom);
 
 const gui = new GUI();
-gui.add(lights, "numLights")
-    .min(1)
-    .max(Lights.maxNumLights)
-    .step(1)
-    .onChange(() => {
-        lights.updateLightSetUniformNumLights();
-    });
 
-const stage = new Stage(scene, lights, camera, stats);
+const stage = new Stage(scene, camera, stats);
 
 var renderer: Renderer | undefined;
 
@@ -67,19 +56,13 @@ function setRenderer(mode: string) {
     renderer?.stop();
 
     switch (mode) {
-        case renderModes.naive:
-            renderer = new NaiveRenderer(stage);
-            break;
-        case renderModes.clusteredDeferred:
-            renderer = new ClusteredDeferredRenderer(stage);
-            break;
         case renderModes.pathtracer:
             renderer = new Pathtracer(stage);
             break;
     }
 }
 
-const renderModes = { naive: "naive", clusteredDeferred: "clustered deferred", pathtracer: "pathtracer" };
+const renderModes = { pathtracer: "pathtracer" };
 let renderModeController = gui.add({ mode: renderModes.pathtracer }, "mode", renderModes);
 renderModeController.onChange(setRenderer);
 
